308c78bc9d77dee8bae23ae2c7bf672826c40d60
menachem margolis <margolismendel5728@gmail.com>
2021-02-22 18:41:27 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return (
+        <button onFocus = {e => console.log('Good!')} onBlur ={e=> console.log('Hey! Eyes on me!') }>Eyes on me</button>
+    )
+}
+export default EyesOnMe;
+
+
+// In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listens for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    return(
+ <input type="password" onChange={ e => console.log('Entering password...')}></input> )
+    
+    
+}
+export default Keypad;
+
+
+
